a7a061154da518106ae85ddfe89b5e71bccdd4bb
sehee <shdesign0820@gmail.com>
2025-05-28 14:58:34 +0900

    DesignHotelsUpdate0528

--- 디자인호텔스레이아웃/js/script.js
+++ 디자인호텔스레이아웃/js/script.js
@@ -43,3 +43,78 @@ $(function () {
       }, 5000);
     });
   });
+
+  $(function () {
+    let currentIndex1 = 0;
+    let section3Slide=$('.section3-right .item-contents li');
+    const maxIndex1 = section3Slide.length;
+
+    // 전환 함수
+    function showSlide1(index) {
+      // 오른쪽 이미지 페이드 전환
+      section3Slide.fadeOut(500);
+      section3Slide.eq(index).fadeIn(500);
+
+      // 왼쪽 이미지 전환
+      $('.section3-left .contentBox > div').hide();
+      $('.section3-left .contentBox > div').eq(index).show();
+
+      // 탭 활성화
+      $('.tab ul li').removeClass('active');
+      $('.tab ul li').eq(index).addClass('active');
+
+      currentIndex1 = index;
+    }
+
+    // 자동 슬라이드
+    let slideTimer1 = setInterval(function () {
+      let nextIndex = (currentIndex1 + 1) % maxIndex1;
+      showSlide1(nextIndex);
+    }, 3000);
+
+    // 탭 클릭 시 수동 전환
+    $('.tab ul li').on('click', function (e) {
+      e.preventDefault();
+      let index = $(this).index();
+      showSlide1(index);
+
+      // 타이머 초기화 후 다시 시작
+      clearInterval(slideTimer1);
+      slideTimer1 = setInterval(function () {
+        let nextIndex = (currentIndex1 + 1) % maxIndex1;
+        showSlide1(nextIndex);
+      }, 5000);
+    });
+  });
+
+$(function () {
+    let currentIndex2 = 0;
+    const maxIndex2 = $('.section4-imgBox>div').length;
+
+    // 전환 함수
+    function showSlide2(index) {
+      // 왼쪽 이미지 페이드 전환
+      $('.section4-imgBox>div').fadeOut(500);
+      $('.section4-imgBox>div').eq(index).fadeIn(500);
+
+      // 탭 활성화
+      $('.section4-txtBox>div span').removeClass('active');
+      $('.section4-txtBox>div span').eq(index).addClass('active');
+
+      currentIndex2 = index;
+    }
+
+    // 자동 슬라이드
+    let slideTimer = setInterval(function () {
+      let nextIndex = (currentIndex + 1) % maxIndex;
+      showSlide(nextIndex);
+    }, 3000);
+
+    // 탭 클릭 시 수동 전환
+    $('.section4-txtBox>div').on('click', function (e) {
+      e.preventDefault();
+      let index = $(this).index();
+      showSlide2(index);
+
+    });
+  });
